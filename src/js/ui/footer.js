9fa70ec41bcdac99478b2b05ed47e6238956be5a
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-11-07 23:32:07 +0100

    Test again

--- src/js/ui/footer.js
+++ src/js/ui/footer.js
@@ -3,8 +3,8 @@ import { load } from '../storage/load.js';
 import { profileButton } from '../templates/profile/index.js';
 
 export const footer = () => {
-  const footer = document.querySelector('footer');
-  const actions = footer.querySelector('#footerActions');
+  const footerElement = document.querySelector('footer'); // Change the variable name for clarity
+  const actions = footerElement.querySelector('#footerActions'); // Change the variable name for clarity
 
   if (isLoggedIn()) {
     const profile = load('profile');
