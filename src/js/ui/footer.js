2417bd69ba202279c4d7521a907ad40fc0837f44
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-11-07 22:47:11 +0100

    Testing eslint on commit

--- src/js/ui/footer.js
+++ src/js/ui/footer.js
@@ -3,7 +3,7 @@ import { load } from '../storage/load.js';
 import { profileButton } from '../templates/profile/index.js';
 
 export const footer = () => {
-  const footer = document.querySelector('footerHello123');
+  const footer = document.querySelector('footer');
   const actions = footer.querySelector('#footerActions');
 
   if (isLoggedIn()) {
