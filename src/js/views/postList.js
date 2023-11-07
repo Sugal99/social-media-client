fce6bc0a62df813ab6100a819343298257b210d5
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-11-07 08:07:11 +0100

    Last Test

--- src/js/views/postList.js
+++ src/js/views/postList.js
@@ -1,7 +1,7 @@
 import { postThumbnailTemplate } from '../templates/index.js';
 
 export const postList = (posts, preview = false) => {
-  const element = document.createElement('div');
+  const element = document.createElement('div12313123');
   element.classList.add('post', 'list');
   element.append(...posts.map((post) => postThumbnailTemplate(post, preview)));
   return element;
