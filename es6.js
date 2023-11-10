cb8ed82e975fe3eb03114df2d101d43a1c199085
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-11-11 00:12:10 +0100

    Creating READ.MD Config

--- es6.js
+++ es6.js
@@ -1,4 +1,4 @@
-export function wait(ms = 1000, resolveWith = "Hello World") {
+export function wait(ms = 1000, resolveWith = 'Hello World') {
   return new Promise((resolve) => {
     setTimeout(() => resolve(resolveWith), ms);
   });
