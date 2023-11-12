00bff2f16fef12b6943a3605f4dcc2be75f98cda
SugalAden <112473563+SugalAden@users.noreply.github.com>
2023-11-12 03:12:20 +0100

    Test

--- src/js/ui/footer.js
+++ src/js/ui/footer.js
@@ -1,16 +1,13 @@
 import { isLoggedIn } from '../api/index.js';
 import { load } from '../storage/load.js';
 import { profileButton } from '../templates/profile/index.js';
-console.log(footer);
-console.log(footer);
+
 export const footer = () => {
   const footer = document.querySelector('footer');
   const actions = footer.querySelector('#footerActions');
-  console.log(footer);
 
   if (isLoggedIn()) {
     const profile = load('profile');
     actions.prepend(profileButton(profile));
-    console.log(profile);
   }
 };
